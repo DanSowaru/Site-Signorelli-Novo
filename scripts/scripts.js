e616795c628bfa134f6fcec5a69d38b27ae8c54c
Danilo Soares <danilo.mota.soares@gmail.com>
2022-03-24 18:12:53 -0300

    mobile and desktop adjustments, testimonials carrousel working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,24 @@
-(function () {
-	"use strict";
-
-	var carousels = function () {
-		$(".owl-carousel1").owlCarousel({
-			loop: true,
-			center: true,
-			margin: 0,
-			responsiveClass: true,
-			nav: false,
-			responsive: {
-				0: {
-					items: 1,
-					nav: false,
-				},
-				680: {
-					items: 2,
-					nav: false,
-					loop: false,
-				},
-				1000: {
-					items: 3,
-					nav: true,
-				},
-			},
-		});
-	};
-
-	(function ($) {
-		carousels();
-	})(jQuery);
-})();
+// Carrousel
+$(document).ready(function () {
+	var silder = $(".owl-carousel");
+	silder.owlCarousel({
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: false,
+		items: 1,
+		stagePadding: 20,
+		center: true,
+		nav: false,
+		margin: 50,
+		dots: true,
+		loop: true,
+		responsive: {
+			0: { items: 1 },
+			480: { items: 2 },
+			575: { items: 2 },
+			768: { items: 2 },
+			991: { items: 3 },
+			1200: { items: 4 },
+		},
+	});
+});
