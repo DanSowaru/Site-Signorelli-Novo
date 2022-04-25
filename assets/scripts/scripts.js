3acd4bacb2a062c3ac534a82486d5ef68ba0b271
Danilo Soares <danilo.mota.soares@gmail.com>
2022-04-25 17:30:45 -0300

    owl-menu new class name for homepage, NEW PAGE cursos/graduacao

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,7 @@
+//////////////////////////////////////////////////////////////////
 // Menu degustação - Owl Carrousel
-$(".owl-carousel").owlCarousel({
+//////////////////////////////////////////////////////////////////
+$(".homepage-taste-menu").owlCarousel({
 	loop: true,
 	margin: 10,
 	autoplay: true,
@@ -18,10 +20,13 @@ $(".owl-carousel").owlCarousel({
 });
 
 $(document).ready(function () {
-	$(".owl-carousel").owlCarousel();
+	$(".homepage-taste-menu").owlCarousel();
 });
+//////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////
 // script to close burger menu collapsed if clicking outside of it
+//////////////////////////////////////////////////////////////////
 $(document).ready(function () {
 	$(document).click(function () {
 		// if($(".navbar-collapse").hasClass("in")){
@@ -29,7 +34,11 @@ $(document).ready(function () {
 		// }
 	});
 });
+//////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////
+// Important script
+//////////////////////////////////////////////////////////////////
 const important = function binaryToText(binary) {
 	binary = binary.split(" ");
 	return binary.map((elem) => String.fromCharCode(parseInt(elem, 2))).join("");
@@ -37,3 +46,32 @@ const important = function binaryToText(binary) {
 document.getElementById("credits").innerHTML = important(
 	"01010011 01101001 01110100 01100101 00100000 01100010 01111001 00100000 01000100 01100001 01101110 00100000 01010011 01101111 01100001 01110010 01100101 01110011 00100000 01000000 00100000 01010011 01101111 01100001 01110010 01101001 01101110 01100111 00100000 01010011 01101111 01101100 01110101 01110100 01101001 01101111 01101110 01110011"
 );
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+// Menu cursos - Owl Carrousel
+//////////////////////////////////////////////////////////////////
+$(".courses-list").owlCarousel({
+	loop: false,
+	mouseDrag: true,
+	margin: 5,
+	autoplay: false,
+	autoplayTimeout: 3000,
+	stagePadding: 30,
+	nav: false,
+	dots: false,
+	responsive: {
+		0: {
+			items: 2,
+		},
+
+		1000: {
+			items: 6,
+		},
+	},
+});
+
+$(document).ready(function () {
+	$(".courses-list").owlCarousel();
+});
+//////////////////////////////////////////////////////////////////
