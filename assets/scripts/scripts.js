b4072d8e585ccf50e99149f99b8ddec46aa65d91
Danilo Soares <danilo.mota.soares@gmail.com>
2022-04-28 18:20:18 -0300

    Filter button created

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -37,7 +37,9 @@ $(document).ready(function () {
 //////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 // Important script
+//////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 const important = function binaryToText(binary) {
 	binary = binary.split(" ");
@@ -49,7 +51,9 @@ document.getElementById("credits").innerHTML = important(
 //////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 // Menu cursos - Owl Carrousel
+//////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 $(".courses-list").owlCarousel({
 	loop: false,
@@ -74,4 +78,59 @@ $(".courses-list").owlCarousel({
 $(document).ready(function () {
 	$(".courses-list").owlCarousel();
 });
+//////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// Menu cursos - Filter
+//////////////////////////////////////////////////////////////////
+
+filterSelection("all");
+function filterSelection(c) {
+	var x, i;
+	x = document.getElementsByClassName("filterDiv");
+	if (c == "all") c = "";
+	// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+	for (i = 0; i < x.length; i++) {
+		w3RemoveClass(x[i], "show");
+		if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+	}
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) == -1) {
+			element.className += " " + arr2[i];
+		}
+	}
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		while (arr1.indexOf(arr2[i]) > -1) {
+			arr1.splice(arr1.indexOf(arr2[i]), 1);
+		}
+	}
+	element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+	btns[i].addEventListener("click", function () {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
+}
+
 //////////////////////////////////////////////////////////////////
